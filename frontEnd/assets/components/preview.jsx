d608934068b5d9760ebf47d16d0123db69f8dbcd
RobertCorreaT <ranireto@gmail.com>
2025-03-09 22:48:14 -0300

    avances => - creación de carpeta backEnd

--- frontEnd/assets/components/preview.jsx
+++ frontEnd/assets/components/preview.jsx
@@ -3,7 +3,7 @@ const Preview = ({formData}) => {
     return (
             <div
                 id={'preview'}
-                className={'bg-red-50 w-3/4 h-3/4 m-auto'}>
+                className={'bg-red-50 w-2/3 m-auto'}>
                 <h2 className={'text-lg'}>{formData.nombreEntrada || "Nombre Entrada"}</h2>
                 <h2 className={'text-xl'}>{formData.nombrePlatoFondo || "Nombre del Plato de Fondo"}</h2>
                 <h2 className={'text-4xl'}>{formData.nombrePostre || "Nombre del Postre"}</h2>
