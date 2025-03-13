a7bf4d0335462dbf864a18444c661c88cb6d3e95
RobertCorreaT <ranireto@gmail.com>
2025-03-13 19:04:21 -0300

    Se estila la preview del archivo. Primera plantilla lista para generar contenido.

--- frontEnd/assets/components/preview.jsx
+++ frontEnd/assets/components/preview.jsx
@@ -1,12 +1,39 @@
-const Preview = ({formData}) => {
+import logoTCG from '/frontEnd/assets/imgs/logoTCG.png'
+
+const Preview = ({dataForm}) => {
 
     return (
             <div
                 id={'preview'}
-                className={'bg-red-50 w-2/3 m-auto'}>
-                <h2 className={'text-lg'}>{formData.nombreEntrada || "Nombre Entrada"}</h2>
-                <h2 className={'text-xl'}>{formData.nombrePlatoFondo || "Nombre del Plato de Fondo"}</h2>
-                <h2 className={'text-4xl'}>{formData.nombrePostre || "Nombre del Postre"}</h2>
+                className={' m-auto rounded shadow '}>
+                <p className={'nerko-one-regular text-[#f9d876] text-lg mt-5'}>Oferta del día</p>
+                <h1 className={'text-white redondaExtraBold text-2xl'}>{dataForm.titulo || "Nombre del producto"}</h1>
+                <p className={'text-white redonda'}>{dataForm.subtitulo || "Detalles"}</p>
+
+                <img
+                    src={dataForm.imagenProducto}
+                    alt="Vista previa imagen"
+                    className={'bg-red-50 w-36 h-36 rounded-lg my-3 m-auto object-center object-cover border-2 border-white'}
+                />
+
+                <div
+                    className={'flex items-center justify-center text-xd text-white redonda'}>
+                    <h4>Precio Normal: $</h4>
+                    <h4>{dataForm.precioNormal}</h4>
+                </div>
+
+                <div
+                    className="flex items-center justify-center text-xl redondaExtraBold">
+                    <h2 className={'text-white'}>Precio Oferta: $</h2>
+                    <h2 className={'text-white'}>{dataForm.precioOferta}</h2>
+                </div>
+
+                <img
+                    src={logoTCG}
+                    alt="logoTCG"
+                    className="w-12 m-auto mt-4"
+                />
+
             </div>
     )
 }
