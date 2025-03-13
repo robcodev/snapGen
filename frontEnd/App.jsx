266d3f522c1d525f0e3f02777c327600bd327aa4
RobertCorreaT <ranireto@gmail.com>
2025-03-13 19:05:35 -0300

    Se suma al manejador de estados la imagen, para ser visualizada en tiempo real

--- frontEnd/App.jsx
+++ frontEnd/App.jsx
@@ -1,15 +1,35 @@
 import './App.css';
 import Form from "./assets/components/form.jsx";
 import Preview from "./assets/components/preview.jsx";
-import React, {useState} from "react";
+import {useState} from "react";
 import TestComponent from "../backEnd/src/components/TestComponents.jsx";
 function App(){
 
-    const [formData, setFormData] = useState({
-        nombreEntrada: "",
-        nombrePlatoFondo: "",
-        nombrePostre: ""
+    function formatearPrecios(numero){
+        return Number(numero).toLocaleString('es-CL');
+    }
+
+    const [dataForm, setDataForm] = useState({
+        titulo: "",
+        subtitulo: "",
+        descripcion: "",
+        precioNormal: "",
+        precioOferta: "",
+        imagenProducto: "",
+        imagenFondo: "",
+        logoProducto: ""
     });
+
+    const handleImageChange = (e) => {
+        const file = e.target.files[0];
+        if (file){
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setDataForm(prevData => ({...prevData, imagenProducto: reader.result}))
+            };
+            reader.readAsDataURL(file);
+        }
+    }
 
     return <>
         <header>
@@ -21,19 +41,20 @@ function App(){
             </nav>
         </header>
 
-        <h1>CRUD Test</h1>
-        <TestComponent/>
 
         <section
-            className={'grid grid-cols-12 h-196 mt-4'}>
-            <div className={'col-span-4 bg-gray-100 items-center justify-center p-5'}>
-                <Form setFormData={setFormData}/>
+            className={'grid grid-cols-1 md:grid-cols-3 mt-4 h-full gap-4'}>
+
+            <div className={'col-span-1 bg-gray-100 items-center justify-center p-5 rounded-lg'}>
+                <Form setDataForm={setDataForm} imageChange={handleImageChange} />
             </div>
 
-            <div className={'col-span-7 bg-gray-200  flex items-center'}>
-                <Preview formData={formData}/>
+            <div className={'col-span-1 md:col-span-2 bg-gray-100 p-10 flex items-center justify-center rounded-lg'}>
+                <Preview dataForm={dataForm} />
             </div>
         </section>
+
+        <TestComponent/>
 
 
     </>
