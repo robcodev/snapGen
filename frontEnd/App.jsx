c896558d0e8e670ccccba5da94ef8e6da94cdafc
RobertCorreaT <ranireto@gmail.com>
2025-03-13 23:04:48 -0300

    feat: implementación de html2canvas para exportar vista previa como imagen

--- frontEnd/App.jsx
+++ frontEnd/App.jsx
@@ -35,7 +35,7 @@ function App(){
         const canva = document.getElementById('canva');
 
         const canvas = await html2canvas(canva, {
-            scale: 2,
+            scale: 4,
             useCORS: true,
         });
         const dataURL = canvas.toDataURL("image/png");
